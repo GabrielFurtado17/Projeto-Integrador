c216f1fc064f8fc1225a7ef393680d2123f00af4
GabrielFurtado17 <43618559+GabrielFurtado17@users.noreply.github.com>
2018-10-09 16:31:13 -0300

    Update qtdfumada.php

--- projetoIntegrador/backend/usuario/qtdfumada.php
+++ projetoIntegrador/backend/usuario/qtdfumada.php
@@ -7,12 +7,27 @@
         $strRecebido = file_get_contents('php://input'); // pega parada
         // echo($strRecebido); // to testando a parada que veio, pra ver se veio mesmo
         $objRecebido = json_decode($strRecebido);// tirando json
-        // echo($objRecebido);
+
         try{
-            $sql = 'SELECT dataSaida as data, COUNT(dataSaida) as qtd  FROM saidacigarro sc
-                INNER JOIN usuario u 
-                    ON u.idusuario = sc.idusuario
-            WHERE MONTH(:sendDate) AND u.idusuario =:idusuario GROUP BY DAY(dataSaida)';
+            // $sql = 'SELECT dataSaida as data, COUNT(dataSaida) as qtd  FROM saidacigarro sc
+            //     INNER JOIN usuario u 
+            //         ON u.idusuario = sc.idusuario
+            // WHERE MONTH(:sendDate) AND u.idusuario =:idusuario GROUP BY DAY(dataSaida)';
+
+            $sql = "
+                SELECT 
+                    dataSaida as `data`,
+                    COUNT(dataSaida) AS qtd
+                FROM 
+                    saidacigarro
+                WHERE 
+                    1
+                    AND dataSaida >= :sendDate
+                    AND MONTH(dataSaida) < (MONTH(:sendDate)+1)
+                    AND idusuario = :idusuario
+                GROUP BY
+                    DAY(dataSaida)
+            ";
             $statement = $banco->prepare($sql);
             $statement->execute([
                 "idusuario" => $_SESSION['idusuario'],
@@ -22,7 +37,7 @@
             // echo ($_SESSION['qtdMediaFumada']);
             die (json_encode((object)[
                 'media'=>$_SESSION['qtdMediaFumada'],
-                'preco'=>$_SESSION['preco']
+                'preco'=>$_SESSION['preco'],
                 'result'=>$result]));
         }catch(Exception $erro) {
             die("algo deu errado parceiro $erro");
